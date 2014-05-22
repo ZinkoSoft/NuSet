1130f38173c612f8d6807dbf2aa61aee04fd826a
James Zinkovitch <pfcmpguy84@hotmail.com>
2014-05-22 00:33:43 -0400

    Initial check-in of a default Visual Studio Extension project. Good start :)

---
+++ NuSet/NuSet/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NuSet")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("James Zinkovitch")]
+[assembly: AssemblyProduct("NuSet")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("NuSet_IntegrationTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010051acdd3fbdc7ba4a2543ead01311b2da04375685176e0489029007eedb18440d1fb35baee1b7d8af2c84ca069a557febf911e9859b569d687f0d6ba4f660f1865abf2e9be7bd9c4591485fbcd81ca60632198641dc430587318d22c0f06a79ebb0bfddc246f6ee23ee1592a6916fac5ba1190f9c5917391139396def9ca97e8e")]
+[assembly: InternalsVisibleTo("NuSet_UnitTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010051acdd3fbdc7ba4a2543ead01311b2da04375685176e0489029007eedb18440d1fb35baee1b7d8af2c84ca069a557febf911e9859b569d687f0d6ba4f660f1865abf2e9be7bd9c4591485fbcd81ca60632198641dc430587318d22c0f06a79ebb0bfddc246f6ee23ee1592a6916fac5ba1190f9c5917391139396def9ca97e8e")]
